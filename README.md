fbcc3b90d09764799060983151e178c164df4b10
Ace <ace.github@gmail.com>
2014-07-19 12:28:57 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Daily Task List
+#### A simple Angular.js task list app
+---
+
+This is a quick weekend experimentation with Angular.js, although I can now see a large number of both new projects and modifications to existing ones that can be done with this framework.
---
+++ static/js/script.js
@@ -0,0 +1,22 @@
+var app = angular.module('Todo', []);
+
+app.controller('TodoCtrl', function($scope) {
+	$scope.todos = [
+		'Add items to todo list',
+	];
+
+	$scope.done = function(todo) {
+		var indexOf = $scope.todos.indexOf(todo);
+		if (indexOf !== -1) {
+			$scope.todos.splice(indexOf, 1, 'Task Completed');
+		}
+	};
+
+	$scope.add = function(e) {
+		if (e.which && e.which === 13) {
+			$scope.todos.push($scope.newTodo);
+			$scope.newTodo = '';
+		}
+	};
+  
+});
