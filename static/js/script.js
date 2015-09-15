77827c886ba3682f6d1b9fadb22e2e1bcf36c04b
Ace <ace.github@gmail.com>
2015-09-14 17:58:06 -0700

    Put Angular code into IIFE

--- static/js/script.js
+++ static/js/script.js
@@ -1,27 +1,30 @@
-var app = angular.module('Todo', []);
+var Main = (function() {
 
-app.controller('TodoCtrl', function($scope) {
-	$scope.todos = [
-		'Review flash cards',
-		'Watch at least 1 MOOC video',
-		'Push at least 1 commit to Github',
-		'Write for at least 30 minutes',
-		'Read at least 50 pages',
-		'Exercise'
-	];
+    var app = angular.module('Todo', []);
 
-	$scope.done = function(todo) {
-		var indexOf = $scope.todos.indexOf(todo);
-		if (indexOf !== -1) {
-			$scope.todos.splice(indexOf, 1, 'Task completed');
-		}
-	};
+    app.controller('TodoCtrl', function($scope) {
+        $scope.todos = [
+            'Review flash cards',
+            'Watch at least 1 MOOC video',
+            'Push at least 1 commit to Github',
+            'Write for at least 30 minutes',
+            'Read at least 50 pages',
+            'Exercise'
+        ];
 
-	$scope.add = function(e) {
-		if (e.which && e.which === 13) {
-			$scope.todos.push($scope.newTodo);
-			$scope.newTodo = '';
-		}
-	};
-  
-});
+        $scope.done = function(todo) {
+            var indexOf = $scope.todos.indexOf(todo);
+            if (indexOf !== -1) {
+                $scope.todos.splice(indexOf, 1, 'Task completed');
+            }
+        };
+
+        $scope.add = function(e) {
+            if (e.which && e.which === 13) {
+                $scope.todos.push($scope.newTodo);
+                $scope.newTodo = '';
+            }
+        };
+
+    });
+})();
